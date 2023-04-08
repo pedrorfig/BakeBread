04d987fe1e3ee9c4e5acb4f6854f70c0216ad8be
Pedro Figueiredo <figueiredor.pedro@gmail.com>
2023-04-08 18:45:52 -0300

    Adding Bootstrap visuals and changing default recipe

--- archive/actions.py
+++
@@ -1,6 +0,0 @@
-
-def ask_to_continue():
-    while True:
-        # some code here
-        if input('Do You Want To Continue? (y/n)') == 'y':
-            break
--- helper.py
+++ helper.py
@@ -1,28 +1,33 @@
-import numpy as np
-
-recipes = {"fried egg": {'type': 'Breakfast',
-                         'description': "Egg fried in butter",
-                         'ingredients': {"Butter": '10g', "Egg": '2 units', "Salt": '2g'},
+recipes = {"Sourdough Bread": {'type': 'Savory',
+                         'description': "Regular Sourdough Bread",
+                         'ingredients': {"All-purpose flour": "500 g", "Sourdough starter": "100 g",
+                                         "Filtered water": '350 g', "Salt": '10 g', },
                          'instructions': {
-                             "Melt butter in pan over medium-low heat": "-",
-                             "Crack the egg into the buttered pan": "-",
-                             "Sprinkle the pinch of salt onto cooking egg": "-",
-                             "Flip egg after about a minute and a half": "-",
+                             "Mix flour with water until there are no lumps": "3 min",
+                             "Wait for the flour to fully absorb the water (autolysis)": "30 min",
+                             "Add the sourdough stater and salt": " 3 min",
+                             "Give it some rest": "20 min",
+                             "Mix it well, by hand or with a stand-mixer": "10 min",
+                             "1st bulk fermentation step": "60 min",
+                             "1st stretch and fold": "2 min",
+                             "2nd bulk fermentation step": "60 min",
+                             "2nd stretch and fold on all four sides of the dough": "2 min",
+                             "3rd bulk fermentation step": "60 min",
+                             "3rd stretch and fold on all four sides of the dough": "2 min",
+                             "4th bulk fermentation step": "60 min",
+                             "Dust the top counter with flour and pour the dough gently": "1 min",
+                             "Stretch and fold on all four sides of the dough, turn it upside down": "1 min",
+                             "Tighten the dough by pushing it on all sides, trying to create a ball-like shape. \
+                             Don't tighten it too much, the gluten network doesn't break": "2 min",
+                             "Give it some rest": "20 min",
+                             "Stretch the dough, making it into a rectangular shape. Fold the two largest sides on top of each other."
+                             "Turn the dough in 90 degrees and roll it, always adding tension, until there is no more dough to roll. ": "3 min",
+                             "Transfer your dough to your duste banetton": "1 min",
+                             "Let it proof": " 30 min",
+                             "Cover it with a plastic bag and let it rest over night in the fridge": "12 h"
                          },
-                         'comments': ["Yummy!!", "Egg-cellent ;->"]
-                         },
-           "buttered toast": {'type': 'Breakfast',
-                              'description': "Toasted bread spread with butter",
-                              'ingredients': {'Butter': '10g', 'Bread': '2 slices'},
-                              'instructions': {
-                                  "Put the bread in the toaster": 1,
-                                  "Take the toast out of the toaster": "-",
-                                  "Put the pad of butter on the toasted bread": "-",
-                                  "Wait for the butter to meat": 1,
-                                  "Spread the melted butter onto the bread": "-"
-                              },
-                              'comments': ["Toasty", "What a great recipe!"]
-                              }
+                         'comments': ["Mouth watering"]
+                         }
            }
 
 def add_ingredients(recipe_name=None, text=None):
